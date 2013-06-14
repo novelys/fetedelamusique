bdc8acb2d6e67f6bdceeba4fef3fea2a28b28323
Yann Klis <yann.klis@novelys.com>
2013-06-14 10:27:35 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fetedelamusique::Application.config.secret_key_base = '5f909034f0725dc11c4b1dbb215b8c6aafa71f7b86447105e3970c9e2377d7869213c3c2a337c17fbfe10e122ea4f004ed8928f8f77806723ebeefac630a9844'
