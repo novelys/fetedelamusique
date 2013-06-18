3238d7ec7f2de3d1412173923d8a11585df23fe6
Soulou <leo@unbekandt.eu>
2013-06-18 17:24:56 +0200

    Add Datetimepicker to new concert form

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,7 +1,5 @@
 class ConcertsController < ApplicationController
-
   def index
-
     if params[:lat].present? && params[:lng].present?
       coordinates = [params[:lat].to_f, params[:lng].to_f]
       @concerts = Concert.near(coordinates, 0.010, :units => :km).sort_by(&:time_start)
@@ -10,16 +8,12 @@ class ConcertsController < ApplicationController
     end
 
     respond_to do |format|
-
       format.html
       format.json {
-
         body = render_to_string :template => "/concerts/index.html.haml", :collection => @concerts, :layout => false
-
         render :json => {body: body}.to_json
       }
     end
-
   end
 
   def show
@@ -30,11 +24,18 @@ class ConcertsController < ApplicationController
   end
 
   def create
-    @concert = Concert.new params[:concert]
+    @concert = Concert.new create_params
 
-    @concert.save
-
-    render :action => "new"
+    if !@concert.save
+      render :action => "new"
+    else
+      redirect_to root_path
+    end
   end
 
+  protected
+
+  def create_params
+    params.require(:concert).permit(:artist, :artist_url, :genre, :description, :venue, :time_start, :time_end)
+  end
 end
